9b919a0c1c8f89d42b6cff0b759d6982e6f8389d
ponty <titi@pince.(none)>
2011-10-24 13:10:24 +0200

    fix test

--- pysimavr/examples/test_example.py
+++ pysimavr/examples/test_example.py
@@ -1,10 +1,10 @@
 ''' unit test example'''
 
-    from pysimavr.sim import ArduinoSim
+from pysimavr.sim import ArduinoSim
+
+def test_atmega88():
+    mcu = 'atmega88'
+    snippet = 'Serial.print("hi");'
     
-    def test_atmega88():
-        mcu = 'atmega88'
-        snippet = 'Serial.print("hi");'
-        
-        output = ArduinoSim(snippet=snippet, mcu=mcu, timespan=0.01).get_serial()
-        assert output == 'hi'
+    output = ArduinoSim(snippet=snippet, mcu=mcu, timespan=0.01).get_serial()
+    assert output == 'hi'
