e765ee16ec3efd92ca5389ebe2d3bfc90fa8130b
Vaclav Klecanda <vencax77@gmail.com>
2011-11-19 16:26:31 +0100

    Udp reader reworked based on asyncore library. It now lets to implement an UART connected device in easay way.

--- pysimavr/udpreader.py
+++ pysimavr/udpreader.py
@@ -1,6 +1,9 @@
 import logging
 import socket
 import threading
+import asynchat
+import asyncore
+import struct
 
 #logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
@@ -9,49 +12,77 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 4321
 TIMEOUT = 0.1
 
-# TODO: better implementation needed
-
-class UdpReader():
+class UdpHandler(asynchat.async_chat):
+    """
+    Handler of UDP pipe from AVR simulator. It provides basic network
+    facilities. It shall be subclassed to implement specific behavior.
+    """
     def __init__(self, ip=UDP_IP, port=UDP_PORT, timeout=TIMEOUT):
-        ''
-        self.sock = None
-        self._stop_thread = False
-        self._thread = None
-        self._buffer = []
+        asynchat.async_chat.__init__(self)
+        self.set_terminator(1)
+        self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.settimeout(timeout)
+        self.socket.setblocking(False)
+        self.ibuffer = ''
         self.ip = ip
         self.port = port
-        self.timeout = timeout
         
     def start(self):
-        self.sock = socket.socket(socket.AF_INET, # Internet
-                              socket.SOCK_DGRAM) # UDP
-        self.sock.sendto('', (self.ip, self.port))
-        
-        self.sock.setblocking(False)
-        self.sock.settimeout(self.timeout)
-        
-        def target():
-            log.debug('thread start')
-            while not self._stop_thread:
-                try:
-                    data = self.sock.recv(1024) # buffer size is 1024 bytes
-#                    log.debug('recv:%s' % str(data))
-                    if data:
-                        self._buffer.append(data)
-                except socket.error as e: # I get timeout error sometimes
-#                    log.debug('socket error:%s' % str(e))
-                    pass
-            log.debug('thread end')
-        self._thread = threading.Thread(target=target)
+        self.socket.sendto('', (self.ip, self.port))
+        self._thread = threading.Thread(target=lambda: 
+                                          asyncore.loop(timeout=1),
+                                        name='UdpHandlerThread')
         self._thread.start()
-
-    def read(self):        
-        x = self._buffer
-        self._buffer = []
-        return ''.join(x)
     
-    def terminate(self):
-        self._stop_thread = True
-        self._thread.join()
-        self.sock.close()
+    def collect_incoming_data(self, data):
+        self.ibuffer += data
 
+    def terminate(self):
+        self.close()
+        self._thread.join()
+
+
+class UdpReader(UdpHandler):
+    """ This handler does not answer anything. It accumulates data
+    which it receives, i.e. does the same as the original one.
+    """ 
+    def found_terminator(self):
+        pass
+    
+    def read(self):
+        x = self.ibuffer
+        self.ibuffer = ''
+        return ''.join(x)
+      
+      
+class UdpRepeater(UdpHandler):
+    """ This handler repeats everything it receives back. """
+    def __init__(self, ip=UDP_IP, port=UDP_PORT, timeout=TIMEOUT):
+      super(UdpRepeater, self).__init__(ip, port, timeout)
+      self.set_terminator(1)
+      
+    def found_terminator(self):
+      resp = self.ibuffer
+      self.ibuffer = ''
+      self.socket.sendto(resp, (self.ip, self.port))
+      
+
+class HypotheticDevice(UdpHandler):
+  """
+  This class is example how to implement a set of UART connected devices
+  communicating via simple protocol: receives address byte and command and
+  response with 2 bytes of data.
+  """
+  def __init__(self, ip=UDP_IP, port=UDP_PORT, timeout=TIMEOUT):
+    super(UdpRepeater, self).__init__(ip, port, timeout)
+    self.set_terminator(1)
+    self.addr = None
+      
+  def found_terminator(self):
+    if self.addr == None:
+      self.addr = struct.unpack('B', self.ibuffer)
+    else:
+      command = struct.unpack('B', self.ibuffer)
+      response = self.reactOnCommand(self.addr, command)
+      self.send(response)
+      self.addr = None
