68782fa83a80465412872c62fbd0be85a6c1aec1
ponty <titi@mint12.(none)>
2012-02-07 18:42:08 +0100

    fix example

--- pysimavr/examples/simple.py
+++ pysimavr/examples/simple.py
@@ -1,5 +1,8 @@
 from pysimavr.avr import Avr
+from entrypoint2 import entrypoint
 
-avr=Avr(mcu='atmega48',f_cpu=8000000)
-avr.step(1)
-print avr.pc
+@entrypoint
+def run_sim():
+    avr=Avr(mcu='atmega48',f_cpu=8000000)
+    avr.step(1)
+    print avr.pc
