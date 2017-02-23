d180b07022c4a0c24cc8c65e96184aea23cf6e5a
ponty <ponty@home>
2017-02-23 11:12:21 +0100

    test: log

---
+++ tests/test_log.py
@@ -0,0 +1,22 @@
+from nose.tools import eq_
+from pysimavr.swig.simavr import avr_make_mcu_by_name, use_mem_logger, \
+    mem_logger_read_line, avr_terminate
+
+
+def test_swig():
+    eq_(mem_logger_read_line(), '')
+    avr = avr_make_mcu_by_name('atmega48')
+    eq_(mem_logger_read_line(), '')
+    avr_terminate(avr)
+    eq_(mem_logger_read_line(), '')
+
+    use_mem_logger()
+
+    eq_(mem_logger_read_line(), '')
+    avr = avr_make_mcu_by_name('atmega48')
+    eq_(mem_logger_read_line(),
+        'Starting atmega48 - flashend 0fff ramend 02ff e2end 00ff\n')
+    eq_(mem_logger_read_line(), '')
+    eq_(mem_logger_read_line(), '')
+    avr_terminate(avr)
+    eq_(mem_logger_read_line(), '')
