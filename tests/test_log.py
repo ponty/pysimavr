1e25b64d5a65477d23433766ef941c305854eb77
ponty <ponty@home>
2017-02-23 16:11:02 +0100

    test: log should return None if there is no message

--- tests/test_log.py
+++ tests/test_log.py
@@ -4,19 +4,19 @@ from pysimavr.swig.simavr import avr_make_mcu_by_name, use_mem_logger, \
 
 
 def test_swig():
-    eq_(mem_logger_read_line(), '')
+    eq_(mem_logger_read_line(), None)
     avr = avr_make_mcu_by_name('atmega48')
-    eq_(mem_logger_read_line(), '')
+    eq_(mem_logger_read_line(), None)
     avr_terminate(avr)
-    eq_(mem_logger_read_line(), '')
+    eq_(mem_logger_read_line(), None)
 
     use_mem_logger()
 
-    eq_(mem_logger_read_line(), '')
+    eq_(mem_logger_read_line(), None)
     avr = avr_make_mcu_by_name('atmega48')
     eq_(mem_logger_read_line(),
         'Starting atmega48 - flashend 0fff ramend 02ff e2end 00ff\n')
-    eq_(mem_logger_read_line(), '')
-    eq_(mem_logger_read_line(), '')
+    eq_(mem_logger_read_line(), None)
+    eq_(mem_logger_read_line(), None)
     avr_terminate(avr)
-    eq_(mem_logger_read_line(), '')
+    eq_(mem_logger_read_line(), None)
