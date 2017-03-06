a2426fe3285df4a12a74a363693b3fad08085c87
ponty <ponty@home>
2017-03-06 06:53:24 +0100

    test: firmware

---
+++ tests/test_firmware.py
@@ -0,0 +1,37 @@
+from nose.tools import eq_
+from pyavrutils import AvrGcc
+from pysimavr.avr import Avr
+from pysimavr.firmware import Firmware
+
+mcu = 'atmega48'
+
+
+def test_fw_1():
+    cc = AvrGcc(mcu=mcu)
+    cc.build('int main(){}')
+    fw = Firmware(cc.output)
+
+    avr = Avr(mcu=mcu, firmware=fw, f_cpu=8000000)
+    eq_(avr.f_cpu, 8000000)
+    eq_(avr.mcu, 'atmega48')
+
+
+def test_fw_2():
+    cc = AvrGcc(mcu=mcu)
+    cc.build('int main(){}')
+    fw = Firmware(cc.output)
+
+    avr = Avr(mcu=mcu, firmware=fw)
+    eq_(avr.f_cpu, 8000000)
+    eq_(avr.mcu, 'atmega48')
+
+
+def test_fw_3():
+    cc = AvrGcc(mcu=mcu)
+    cc.build('int main(){}')
+    fw = Firmware(cc.output)
+
+    avr = Avr(mcu=mcu, f_cpu=8000000)
+    avr.load_firmware(fw)
+    eq_(avr.f_cpu, 8000000)
+    eq_(avr.mcu, 'atmega48')
