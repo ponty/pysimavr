535b12efbf95a45d82a1913eddd69bfa6243f509
Premik <premysl.srubar@post.cz>
2017-04-04 14:20:43 +0200

    Logger callback

--- pysimavr/swig/utils_wrap.h
+++ pysimavr/swig/utils_wrap.h
@@ -57,4 +57,46 @@ private:
 };
 
 
+class SwigDirector_LoggerCallback : public LoggerCallback, public Swig::Director {
+
+public:
+    SwigDirector_LoggerCallback(PyObject *self);
+    virtual ~SwigDirector_LoggerCallback();
+    virtual void on_log(char const *msg, int const level);
+
+/* Internal director utilities */
+public:
+    bool swig_get_inner(const char *swig_protected_method_name) const {
+      std::map<std::string, bool>::const_iterator iv = swig_inner.find(swig_protected_method_name);
+      return (iv != swig_inner.end() ? iv->second : false);
+    }
+    void swig_set_inner(const char *swig_protected_method_name, bool swig_val) const {
+      swig_inner[swig_protected_method_name] = swig_val;
+    }
+private:
+    mutable std::map<std::string, bool> swig_inner;
+
+#if defined(SWIG_PYTHON_DIRECTOR_VTABLE)
+/* VTable implementation */
+    PyObject *swig_get_method(size_t method_index, const char *method_name) const {
+      PyObject *method = vtable[method_index];
+      if (!method) {
+        swig::SwigVar_PyObject name = SWIG_Python_str_FromChar(method_name);
+        method = PyObject_GetAttr(swig_get_self(), name);
+        if (!method) {
+          std::string msg = "Method in class LoggerCallback doesn't exist, undefined ";
+          msg += method_name;
+          Swig::DirectorMethodException::raise(msg.c_str());
+        }
+        vtable[method_index] = method;
+      }
+      return method;
+    }
+private:
+    mutable swig::SwigVar_PyObject vtable[1];
+#endif
+
+};
+
+
 #endif
